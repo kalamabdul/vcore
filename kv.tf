7212b2d4532fafd7ee89899e47fef7a2b4fdfc5a
Kalam Abdul <kalam.abdul@kalam.abdul-PW073X7Q3M>
2024-04-28 11:55:51 -0400

    from a123

--- kv.tf
+++ kv.tf
@@ -4,3 +4,10 @@ resource "vault_mount" "kvv2" {
   options     = { version = "2" }
   description = "KV Version 2 secret engine mount"
 }
+
+resource "vault_mount" "kvv2a123" {
+  path        = "kvv2a123"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 secret engine mount"
+}
