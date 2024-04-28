813e2983cadaab5ed69d7235af388d6e1b753601
Kalam Abdul <kalam.abdul@kalam.abdul-PW073X7Q3M>
2024-04-28 11:56:54 -0400

    from b234

--- kv.tf
+++ kv.tf
@@ -4,3 +4,10 @@ resource "vault_mount" "kvv2" {
   options     = { version = "2" }
   description = "KV Version 2 secret engine mount"
 }
+
+resource "vault_mount" "kvv2b234" {
+  path        = "kvv2b234"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 secret engine mount"
+}
