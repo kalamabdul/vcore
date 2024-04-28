ce8be946ebaa7c6320495cddb82d77cdbb9edb75
Kalam Abdul <kalam.abdul@kalam.abdul-PW073X7Q3M>
2024-04-28 11:02:03 -0400

    add approle

---
+++ core.tf
@@ -0,0 +1,14 @@
+resource "vault_auth_backend" "approle" {
+  type = "approle"
+}
+
+resource "vault_approle_auth_backend_role" "example" {
+  backend        = vault_auth_backend.approle.path
+  role_name      = "agent"
+  token_policies = ["agent"]
+  secret_id_ttl  = 3000
+  token_num_uses = 80
+  token_ttl  = 25
+  token_max_ttl = 50
+  secret_id_num_uses = 0
+}
