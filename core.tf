af2827a2c66478063f55a83a5a5d973ca1f27d65
Kalam Abdul <kalam.abdul@kalam.abdul-PW073X7Q3M>
2024-04-28 11:25:24 -0400

    add aws from a123

--- core.tf
+++ core.tf
@@ -22,3 +22,12 @@ resource "vault_auth_backend" "example" {
     listing_visibility = "unauth"
   }
 }
+
+resource "vault_auth_backend" "example" {
+  type = "aws"
+
+  tune {
+    max_lease_ttl      = "90000s"
+    listing_visibility = "unauth"
+  }
+}
