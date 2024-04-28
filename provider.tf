e2ca79ba757e8d2be027c171ce29a897bb0c2aeb
Kalam Abdul <kalam.abdul@kalam.abdul-PW073X7Q3M>
2024-04-28 11:00:41 -0400

    add provider

--- provider.tf
+++ provider.tf
@@ -10,10 +10,5 @@ terraform {
 provider "vault" {
   # Configuration options
   address = "https://vault-public-vault-22deb760.8ee49bbe.z1.hashicorp.cloud:8200"
-  token = var.token
   max_lease_ttl_seconds = "180000"
-}
-
-variable "token" {
-  
 }
